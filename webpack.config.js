c50e0bbaee2b471e80ad4d1b8e99cb1cca229228
Jake Wija <jakewija@icloud.com[core]>
2020-06-09 14:38:47 -0700

    ADD @types for react, react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const ENTRY = path.resplve(__dirname, 'src', 'index.js');
-
+const ENTRY = path.resplve(__dirname, 'src', 'src_index.ts');
+const OUT = path.resolve(__dirname, './public');
 
 module.exports = {
   mode: 'development',
   entry: ENTRY,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
-    filename: '.js',
-    path: path.resolve(__dirname, './public'),
-  }
-}
+    filename: 'bundle.js',
+    path: OUT,
+  },
+  devtool: 'inline-source-map',
+};
