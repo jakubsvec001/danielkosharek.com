669e424c0899cab882cf857861f530d5952a5898
Jake Wija <jakewija@icloud.com[core]>
2020-06-11 12:41:13 -0700

    ADD css, scss, sass to webpack, set up initial files for modularized css, and for global css imports

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import StyledHello from '../../components/StyledHello/StyledHello';
+
+const App: React.FC= () => {
+
+  return (
+    <div>
+      <StyledHello />
+      hello
+    </div>
+  )
+}
+
+export default App;
