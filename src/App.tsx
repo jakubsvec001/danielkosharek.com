735664d7d2a640f153de26ca8b19d0c0ac9ca874
Jake Wija <jakewija@icloud.com[core]>
2020-06-10 20:16:40 -0700

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import StyledHello from './components/StyledHello';
 
 const App: React.FC= () => {
 
   return (
-    <h1>hello</h1>
+    <StyledHello />
   )
 }
 
