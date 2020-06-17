a26bd5e72f4f5f01baeba01a0afad4726f1bd169
Jake Wija <jakewija@icloud.com[core]>
2020-06-17 14:08:39 -0700

    UPDATE

---
+++ src/||0||__App/||0||__App_containers/||3||__Modal_container/||0||__Authentication_page/$index__Authentication_page.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as style from '../../src_index.scss';
+
+const Footer_section: React.FC = () => {
+  return (
+    <footer className={style.footer}>
+      Footer
+    </footer>
+  )
+}
+
+export default Footer_section;
---
+++ src/||0||__App/||0||__App_containers/||3||__Modal_container/||0||__Authentication_page/||0||__AuthenticationPage_components/LogIn_view.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Footer_section = () => {
+  return (
+    <footer className={style.footer}>
+      Footer
+    </footer>
+  )
+}
+
+export default Footer_section;
