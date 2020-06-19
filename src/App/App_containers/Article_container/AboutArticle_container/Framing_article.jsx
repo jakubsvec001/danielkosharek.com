d66a39cfaa7f9af03e1d50e9f93fbae4bf37e098
Jakub Svec <jakubsvec001@gmail.com>
2020-06-19 16:07:47 -0700

    MINOR changes, modify flow of notes. Current task is in the middle. Done tasks are at the bottom. Next tasks are at the top.

--- src/App/App_containers/Article_container/AboutArticle_container/Framing_article.jsx
+++ src/App/App_containers/Article_container/AboutArticle_container/Framing_article.jsx
@@ -4,16 +4,13 @@ import styled from 'styled-components';
 import ArtistStatement_component from './ArtistStatement_component';
 import Collections_component from './Collections_component';
 
-
-
-
 const Landing_article = () => {
   return (
     <>
-      <ArtistStatement_component/>
+      <ArtistStatement_component />
       <Collections_component />
     </>
-  )
+  );
 };
 
-export default Landing_article
+export default Landing_article;
