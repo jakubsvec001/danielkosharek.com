46c025fcaf2e53628118e89c094f63182b04c5e9
Jakub Svec <jakubsvec001@gmail.com>
2020-06-24 13:57:07 -0700

    UPDATE component aliases

--- src/App/index__App.jsx
+++ src/App/index__App.jsx
@@ -7,10 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from '../styles/GlobalStyle';
 import { theme } from '../styles/theme';
 
-import Header_container from './App_containers/Header_container/Header';
-import Footer_container from './App_containers/Footer_container/Footer';
-import Page_container from './App_containers/Page_container/Page_container';
-import Modal_container from './App_containers/Modal_container/Modal';
+import Header from './App_containers/Header_container/Header';
+import Footer from './App_containers/Footer_container/Footer';
+import Page from './App_containers/Page_container/Page_container';
+import Modal from './App_containers/Modal_container/Modal';
 
 
 // Define global styles
@@ -50,10 +50,10 @@ const App = () => {
           </Helmet>
           <GlobalStyle />
           <StyledApp>
-            {isModal && <Modal_container />}
-            <Header_container />
-            <Page_container/>
-            <Footer_container />
+            {isModal && <Modal />}
+            <Header />
+            <Page/>
+            <Footer />
           </StyledApp>
         </Fragment>
       </ThemeProvider>
