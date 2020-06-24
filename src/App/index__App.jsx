c5a47212f15947f509a211475ca6b0fe91d6b58e
Jakub Svec <jakubsvec001@gmail.com>
2020-06-24 13:52:56 -0700

    UPDATE naming convention to better reflect semantic structure

--- src/App/index__App.jsx
+++ src/App/index__App.jsx
@@ -7,10 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from '../styles/GlobalStyle';
 import { theme } from '../styles/theme';
 
-import Header_container from './App_containers/Header_container/Header_container';
-import Footer_container from './App_containers/Footer_container/Footer_container';
-import Article_container from './App_containers/Article_container/Article_container';
-import Modal_container from './App_containers/Modal_container/Modal_container';
+import Header_container from './App_containers/Header_container/Header';
+import Footer_container from './App_containers/Footer_container/Footer';
+import Page_container from './App_containers/Page_container/Page_container';
+import Modal_container from './App_containers/Modal_container/Modal';
 
 
 // Define global styles
@@ -52,7 +52,7 @@ const App = () => {
           <StyledApp>
             {isModal && <Modal_container />}
             <Header_container />
-              <Article_container/>
+            <Page_container/>
             <Footer_container />
           </StyledApp>
         </Fragment>
