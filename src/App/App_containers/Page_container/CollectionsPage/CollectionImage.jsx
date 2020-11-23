f68b865200c6c58aa3a12b15f9dfa70a8ee7d50f
Jakub Svec <jakubsvec-laptop@Jakubs-MacBook-Pro.local>
2020-11-23 15:33:37 -0800

    TURN off hanldeModalToggle

--- src/App/App_containers/Page_container/CollectionsPage/CollectionImage.jsx
+++ src/App/App_containers/Page_container/CollectionsPage/CollectionImage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyledImageContainer, StyledCollectionsImage, StyledImageCaption } from './imageStyles';
 
 const CollectionImage = ({ src, title, actions }) => {
+  actions.handleModalToggle = () => {};
+
   return (
     <StyledImageContainer>
       <StyledCollectionsImage src={src} alt="a painting by Daniel Kosharek" onClick={actions.handleModalToggle} />
