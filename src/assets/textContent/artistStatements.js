14179d139ad6fa327bc470c512d69ad93e317a9c
Jakub Svec <jakubsvec-laptop@Jakubs-MacBook-Pro.local>
2020-11-30 16:57:05 -0800

    ADD images

---
+++ src/assets/textContent/artistStatements.js
@@ -0,0 +1,14 @@
+export default {
+  aMeasureOfTime: {
+    title: "A Measure of Time",
+    statement: "Marking the passage of time"
+  },
+  landscape: {
+    title: "Landscapes",
+    statement: "Impressions of discrete locations",
+  },
+  additional:{
+    title: "Additional Paintings",
+    statement: "A smattering of splatterings",
+  },
+};
