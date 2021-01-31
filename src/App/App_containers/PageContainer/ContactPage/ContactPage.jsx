3521da4222065759f106e2b8f412b6f8e056fb34
Jakub Svec <jakubsvec001@gmail.com>
2021-01-30 19:37:44 -0800

    UPDATE artist statements, update Contact page with statement

--- src/App/App_containers/PageContainer/ContactPage/ContactPage.jsx
+++ src/App/App_containers/PageContainer/ContactPage/ContactPage.jsx
@@ -6,6 +6,7 @@ import {
 } from  "../PageStyles";
 
 import { StyledText, StyledTextBackground, StyledFrameHero, StyledContainer, StyledTextContainer } from "./ContactStyles";
+import statements from "../../../../assets/textContent/artistStatements";
 
 const contactHeroSrc = import(
   "../../../../assets/images/additional/additional_1000px/SelfPortrait_1000px.jpg"
@@ -36,9 +37,12 @@ const ContactPage = () => {
         {heroImageComponent}
         <StyledTextContainer>
           <h2>Daniel Kosharek</h2>
-          <StyledText>lives in Santa Fe, NM, where he enjoys baking bread and painting.</StyledText>
+          <StyledText>{statements.personalStatement.statementPart1}</StyledText><hr />
+          <StyledText>{statements.personalStatement.statementPart2}</StyledText><hr />
+          <StyledText>{statements.personalStatement.statementPart3}</StyledText><hr />
+          <StyledText>{statements.personalStatement.statementPart4}</StyledText><hr />
           <hr />
-          <StyledText>For information about sales, contact Daniel at: </StyledText>
+          <StyledText>For information about sales, contact Daniel at: </StyledText><hr/>
           <br />
           <StyledTextBackground><em>danielkosharek (at) gmail (dot) com</em></StyledTextBackground>
         </StyledTextContainer>
--- src/assets/textContent/artistStatements.js
+++ src/assets/textContent/artistStatements.js
@@ -1,7 +1,14 @@
 export default {
+  personalStatement: {
+    title: "Artist statement",
+    statementPart1: "The creative process is a very private and personal experience; I’ve always felt that the finished painting itself has a lot more to say than I do. I like to work in series, to exhaust a certain approach to a subject, and the plasticity of paint and color are the words I use to tell that story.",
+    statementPart2: "And because I rely heavily on the painting speaking to the viewer, I’ve never felt the need to create an artistic manifesto.",
+    statementPart3: "So, listen to the paintings, not me.",
+    statementPart4: "— Daniel Kosharek",
+  },
   aMeasureOfTime: {
     title: "A Measure of Time",
-    statement: "Marking the passage of time"
+    statement: "Marking the passage of time",
   },
   landscape: {
     title: "Landscapes",
